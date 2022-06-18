b1c603c3c8cab492c56293a1f541e10fb7928b43
donghune <reona7140@naver.com>
2022-06-18 16:08:03 +0900

    release 1.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,9 @@
+val kotlinVersion = "1.7.0"
+
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.7.0"
     kotlin("plugin.serialization") version "1.5.21"
     id("maven-publish")
-}
-
-apply(plugin = "com.github.johnrengelman.shadow")
-
-val kotlinVersion = "1.7.0"
-group = "io.github.donghune"
-
-buildscript {
-    repositories {
-        gradlePluginPortal()
-    }
-    dependencies {
-        classpath("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
-    }
 }
 
 java {
@@ -24,35 +12,27 @@ java {
     }
 }
 
-val shade = configurations.create("shade")
-shade.extendsFrom(configurations.implementation.get())
-
-tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
-    archiveFileName.set("${project.name}-${project.property("version")}.jar")
-}
-
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/")
-    maven("https://oss.sonatype.org/content/repositories/snapshots")
-    maven("https://oss.sonatype.org/content/repositories/central")
-    maven("https://maven.enginehub.org/repo/")
-    maven("https://jitpack.io")
+    maven { url = uri("https://repo.papermc.io/repository/maven-public/") }
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-    compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
 
-    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
+    compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
     compileOnly("io.github.monun:kommand-api:2.12.0")
-    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.0-SNAPSHOT")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
 
     compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
     compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
     compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-core:1.3.3")
     compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
 }
+
+val pluginName = rootProject.name.split('-').joinToString("") { it.capitalize() }
 
 publishing {
     repositories {
